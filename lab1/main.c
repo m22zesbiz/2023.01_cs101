6b07a2299e3b485747ed2b8d42d4eaaace5dd5ef
dlghwjd-tw <83802103+dlghwjd-tw@users.noreply.github.com>
2023-03-09 13:57:53 +0800

    Create main.c

---
+++ lab1/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int a[] = {1, 2, 3};
+    char b[] = "ABC";
+    float c[] = {1.1, 1.2, 1.3}; 
+    int arr_read[3]; 
+
+    FILE* fp;
+    fp = fopen("a.bin", "wb+");
+    fwrite(a, sizeof(a), 1, fp);
+    fread(arr_read, sizeof(arr_read), 1, fp);
+
+    for (int i = 0; i < sizeof(arr_read); i++) {
+        printf("%d", arr_read[i]);
+    }
+
+    fclose(fp);
+}
