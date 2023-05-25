5c986f7b0f517bd8b913a2da13172091af78129e
dlghwjd-tw <83802103+dlghwjd-tw@users.noreply.github.com>
2023-05-25 13:22:47 +0800

    Add files via upload

---
+++ lab9/rmain.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std; 
+class replaceMyString {
+public:
+    void replaceString(const string& oldStr, const string& newStr) {
+        string filename = "rmain.cpp";
+        ofstream outputFile(filename);
+        string line;
+        
+        ifstream inputFile("main.cpp");
+        if (inputFile.is_open()) {
+            while (getline(inputFile, line)) {
+                if (!line.empty()) {
+                    size_t pos = line.find(oldStr);
+                    while (pos != string::npos) {
+                        line.replace(pos, oldStr.length(), newStr);
+                        pos = line.find(oldStr, pos + newStr.length());
+                    }
+                    outputFile << line << endl;
+                }
+            }
+            inputFile.close();
+        }
+        outputFile.close();
+    }
+};
+int main() {
+    replaceMyString my; 
+    my.replaceString("IU is best", "IU is best is best"); 
+    return 0; 
+}
